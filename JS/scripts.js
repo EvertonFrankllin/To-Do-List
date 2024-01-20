350abd2e244897ffdfa17a9f4dfbcfdfc1865a07
Everton Frankllin Pereira Silva <98755634+EvertonFrankllin@users.noreply.github.com>
2024-01-20 18:05:28 -0300

    Correção de Bug

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,12 +1,7 @@
 
-const button = document.querySelector('.button-add-task'); // Aqui eu estou acessando a minha classe dentro do HTML
-const input = document.querySelector('.task-inp');// Aqui eu estou acessando a minha classe dentro do HTML
+const button = document.querySelector('.button-add-task');
+const input = document.querySelector('.task-inp');
 
-function addInpPeloButton() {
-    alert('Botão clicado');
-}
-
-button.addEventListener("click", addInpPeloButton);
-//esse evento fica de olho no botão e o evento de click deixa válido apenas quando o botão foin clicado.
-// E toda vez que o botão for clicado eu quero chamar uma função.
-
+button.addEventListener('click', () => {
+    console.log(input.value);
+});
