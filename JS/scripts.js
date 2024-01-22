1e730ee1e70df26d46749fc6c235167953858f7c
Everton Frankllin Pereira Silva <98755634+EvertonFrankllin@users.noreply.github.com>
2024-01-21 21:42:23 -0300

    Aplicação completa
    Nessa versão 01, procurei apenas abordar os conceitos mais básicos de HTML, CSS e JavaScript.
    Está aplicação terá mais 3 atualiazações e portanto o grau de dificuldade almentará.

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,16 +2,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const button = document.querySelector('.button-add-task');
     const input = document.querySelector('.task-inp');
+    const completList = document.querySelector('.list-task');
+    
+
+
+        
+    
 
     let addTaskLi = [];
     
         function addnewTask(){
             addTaskLi.push(input.value);
-            console.log(addTaskLi);
+            input.value ='';
+            openTask();
         };
 
+        function openTask(){
+            let newLi= '';
+            
+            addTaskLi.forEach((task) => {
+                newLi +=  
+                `<li class="task">
+                    <img src="imagens/foguete.png" alt="imagem de foguete">
+                        <p>${task}</p>
+                <img class="lixo" src="./imagens/excluir.png"  alt="Imagem de lixeira" >
+                </li>`
+                
+            });
+            
+            completList.innerHTML = newLi;
+
+        };
+        
+        
     button.addEventListener('click', addnewTask);
+
+    
+   
 });
+
+
+
+
+
+
+
+
 
 
 
